06e6fe0246e1050c1b19fc6862569135ef4aca76
uanx <uanx@10.232.234.134>
2022-12-15 13:18:49 +0800

    commit

---
+++ FinalProject/src/main/java/GoogleQuery.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+
+import java.io.IOException;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class GoogleQuery {
+	public GoogleQuery(String searchKeyword)
+	{
+		//this.searchKeyword = searchKeyword;
+		//this.url = "http://www.google.com/search?q="+searchKeyword+"&oe=utf8&num=20";
+	}
+	
+	private String fetchContent(String url) throws IOException
+	{
+		String retVal = "";
+
+		URL u = new URL(url);
+		URLConnection conn = u.openConnection();
+		//set HTTP header
+		conn.setRequestProperty("User-agent", "Chrome/107.0.5304.107");
+		InputStream in = conn.getInputStream();
+
+		InputStreamReader inReader = new InputStreamReader(in, "utf-8");
+		BufferedReader bufReader = new BufferedReader(inReader);
+		String line = null;
+
+		while((line = bufReader.readLine()) != null)
+		{
+			retVal += line;
+		}
+		return retVal;
+	}
+
+}
