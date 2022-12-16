571aadc7dfdb748874b3853ec8061b9c0f933bc0
Zi Hsin Chiu <110306058@nccu.edu.tw>
2022-12-17 01:40:20 +0800

    revise main

--- FinalProject/src/main/java/Main.java
+++ FinalProject/src/main/java/Main.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 
+//為什麼有些網址會找不到？
+
 public class Main {
 	public static void main(String[] args) throws IOException {
 
